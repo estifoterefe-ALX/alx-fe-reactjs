7eddccaaa9b140c799b8771091c153ccbf0ee6c8
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-10 16:04:36 +0300

    fixes

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
-function mainContent() {
+function MainContent() {
   return (
     <main>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
-export default mainContent;
+export default MainContent;
