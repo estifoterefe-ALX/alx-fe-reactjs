6f2bfc006cd81c946a4372e4ba079b2dd35f6fd2
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-16 11:25:53 +0300

    fixes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import AboutPage from "./components/AboutPage";
-import ContactPage from "./components/ContactPage";
-import ProfilePage from "./components/ProfilePage";
+import HomePage from "./components/Home";
+import AboutPage from "./components/About";
+import ContactPage from "./components/Contact";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -30,7 +29,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
-          <Route path="/service" element={<ProfilePage />} />
+          <Route path="/services" element={<Services />} />
         </Routes>
       </BrowserRouter>
     </>
