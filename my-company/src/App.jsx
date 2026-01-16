f02a8665edf54c5f7fec1f6cf6214ac45cbd9841
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-16 11:39:19 +0300

    fixes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -4,26 +4,12 @@ import HomePage from "./components/Home";
 import AboutPage from "./components/About";
 import ContactPage from "./components/Contact";
 import Services from "./components/Services";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
