ac328f4e117ed65911e02178e3ac62430eab435f
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-16 11:43:17 +0300

    fixes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/Home";
 import AboutPage from "./components/About";
 import ContactPage from "./components/Contact";
@@ -8,7 +8,17 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-<Navbar />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,29 +1,21 @@
 function Navbar() {
   return (
-    <BrowserRouter>
-      <nav>
-        <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-    </BrowserRouter>
+    <nav>
+      <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 export default Navbar;
