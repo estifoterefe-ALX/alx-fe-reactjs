317dfea47533bc10599c44aebb3103aa56093358
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-16 10:14:41 +0300

    fixes

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -7,8 +7,8 @@ function Counter() {
       <p>This is a simple counter component.</p>
       <p>Current Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
-      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
