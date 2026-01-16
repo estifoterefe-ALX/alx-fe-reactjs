cd01daa421a493a049894a336c5b3a96c01811d9
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-16 10:12:44 +0300

    counter

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>Counter Component</h2>
+      <p>This is a simple counter component.</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <button onClick={() => setCount(0)}>reset</button>
+    </div>
+  );
+}
+export default Counter;
