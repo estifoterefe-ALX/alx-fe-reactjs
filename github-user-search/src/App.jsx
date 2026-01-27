6683ba63e0c50c80848c70b592bb1efa687a7308
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-01-27 11:40:02 +0300

    fixes

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Home</div>} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
