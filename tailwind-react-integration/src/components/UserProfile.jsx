3c3cc46e9c569482e4511a12f11976d27b1a8b9e
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-02-05 09:42:38 +0300

    fixes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 max-w-100p mx-auto my-20 border rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 border rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
