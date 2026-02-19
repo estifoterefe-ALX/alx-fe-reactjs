e4c9d20aef8c395b026152d169cc6f47ef5a2ff1
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-02-19 11:45:35 +0300

    task

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,4 @@
+function ProfileDetails() {
+  return <></>;
+}
+export default ProfileDetails;
---
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -0,0 +1,6 @@
+function ProfileSettings (){
+    return(
+        <></>
+    )
+}
+export default ProfileSettings
