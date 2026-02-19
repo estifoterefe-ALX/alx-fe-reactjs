da538aea37b3fd86e60a36d07ccee104a846ab81
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-02-19 11:27:26 +0300

    task

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,6 +1,25 @@
-function PostsComponent (){
-    return(
-        <></>
-    )
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+function PostsComponent() {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => axios.get("https://jsonplaceholder.typicode.com/posts"),
+  });
+  console.log(data);
+  return (
+    <>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : isError ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <ul>
+          {data?.data.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 export default PostsComponent;
