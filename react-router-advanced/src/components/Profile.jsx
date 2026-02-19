5f8b94c7ac1d660d92c040cf77e3d0b2ecf3e6af
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-02-19 11:48:31 +0300

    fixes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 function Profile() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div>Profile Page</div>}>
+          <Route path="/details" element={<ProfileDetails />} />
+          <Route path="/settings" element={<ProfileSettings />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 export default Profile;
