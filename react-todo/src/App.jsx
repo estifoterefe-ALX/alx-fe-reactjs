6057e2c640fb6b21a98287a741c61bfe082fc9bb
Estifo77 <139631617+Estifo77@users.noreply.github.com>
2026-02-20 10:58:53 +0300

    fixes

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,12 +1,10 @@
-
-
+import TodoList from "./components/TodoList";
 function App() {
-
   return (
     <>
-      
+      <TodoList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
